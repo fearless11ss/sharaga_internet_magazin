f42ba9aeea5550dbbb95d0294c38530d306fdd6a
fearless11ss <157283792+fearless11ss@users.noreply.github.com>
2026-02-04 13:13:18 +0300

    .

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('about/', views.about, name='about'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     """Главная страница интернет-магазина AppleStore."""
     return render(request, 'shop/index.html')
+
+
+def catalog(request):
+    """Страница каталога товаров."""
+    return render(request, 'shop/catalog.html')
+
+
+def about(request):
+    """Страница «О магазине»."""
+    return render(request, 'shop/about.html')
